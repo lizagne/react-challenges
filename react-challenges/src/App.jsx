061a747c8faf528f559dcd363511fe3cc18446d2
Liz Verde <>
2017-11-27 12:23:31 +0000

    2nd react component to add a paragraph to the app

--- react-challenges/src/App.jsx
+++ react-challenges/src/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 import Header from "./Header" //importing the header
-
+import Paragraph from "./Paragraph"
 
 const App = () => (
+   <div>
    <Header />
+   <Paragraph />
+   </div>
+
 );
 
 export default App;
